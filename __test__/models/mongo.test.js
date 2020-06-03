0fe08192345cb8f91ae795f62e3250fccded24bc
rowaidsayyed <rowaidsayyed@gmail.com>
2020-06-03 18:55:53 +0300

    finish all

--- __test__/models/mongo.test.js
+++ __test__/models/mongo.test.js
@@ -19,17 +19,9 @@ let testClass = new Modelmongo(testModel);
 
 describe('mongo.js', () => {
 
-  // beforeAll(async () => {
-  //   const url = `mongodb://localhost:27017/testapi`;
-  //   await mongoose.connect(url, { useNewUrlParser: true });
-  // });
-
   afterEach(async () => {
     await testModel.deleteMany();
   });
-  // afterAll(() => {
-  //   return mongoose.disconnect();
-  // });
 
   it('can get() all categories', () => {
     let testObj = { name: 'test 1' };
